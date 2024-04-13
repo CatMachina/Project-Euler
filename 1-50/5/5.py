9ae73dad1b38cb41292213d23e20048e6af8784e
Ryan Zhang <ryanse2000@gmail.com>
2024-04-13 01:30:48 -0400

    5

---
+++ 1-50/5/5.py
@@ -0,0 +1,5 @@
+# Just take the maximum power of primes up to 20 found
+# O(1)
+# For larger numbers, can do sieve of eratosthenes to find primes and factor them easily => O(n log n), n = upper number
+
+print(2 ** 4 * 3 ** 2 * 5 * 7 * 11 * 13 * 17 * 19)
