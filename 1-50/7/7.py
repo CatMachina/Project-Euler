9fe40222c0ce374ac4fc78a7c6ee529e4212ac6d
Ryan Zhang <ryanse2000@gmail.com>
2024-04-13 09:51:22 -0400

    7

---
+++ 1-50/7/7.py
@@ -0,0 +1,22 @@
+# Keep track of list of primes and use to determine if other numbers are prime
+# According to Prime Number Theorem (PNT), # of primes <= n ~ n / log(n)
+# Each iteration of checking can thus approximated as sqrt(x) / log (x) for iteration x, or total
+# O(n * sqrt(n) / log (n)), n = n-th prime needed
+# Alternatively, sieve methods can be used (e.g. sieve of eratosthenes), but an upper bound needs to be known beforehand
+
+n = 10001
+primes = []
+i = 2
+while len(primes) < n:
+  is_prime = True
+  for p in primes:
+    if p * p > i:
+      break
+    if i % p == 0:
+      is_prime = False
+      break
+  if is_prime:
+    primes.append(i)
+  i += 1
+
+print(primes[-1])
